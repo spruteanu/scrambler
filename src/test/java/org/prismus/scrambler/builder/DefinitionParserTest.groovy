70de1ca6940f0c05351660d840657d55728b3267
serge <spruteanu@gmail.com>
2013-12-19 01:23:45 -0800

    0: simplified test cases

--- src/test/java/org/prismus/scrambler/builder/DefinitionParserTest.groovy
+++ src/test/java/org/prismus/scrambler/builder/DefinitionParserTest.groovy
@@ -11,8 +11,17 @@ class DefinitionParserTest extends Specification {
         final parser = new DefinitionParser()
         expect:
         parser != null
-//        parser.parse("'test*' random 1L 100L") != null
-        parser.parse("RandomInteger('myProperty', 1, 100)") != null
+//        parser.parse("'test*' random 1L, 100L") != null
+        parser.parse("random 1, 100") != null
+
+        parser.parse("random 1L, 100L") != null
+        parser.parse("random new Date()") != null
+        parser.parse("random('some template string', 100, true, false)") != null
+
+        parser.parse("RandomInteger 1, 100") != null
+        parser.parse("RandomLong 1, 100") != null
+        parser.parse("RandomString('some template string', 100, true, false)") != null
+
     }
 
 }
