50e09b879ca5199ff52b64a63219ec37d095883c
serge <spruteanu@gmail.com>
2013-12-26 23:45:06 -0800

    0: predicate placeholder

---
+++ src/main/java/org/prismus/scrambler/builder/ValuePredicate.java
@@ -0,0 +1,18 @@
+package org.prismus.scrambler.builder;
+
+/**
+ * todo: add description
+ *
+ * @author Serge Pruteanu
+ */
+class ValuePredicate {
+
+    public boolean apply(String propertyName) {
+        return false;
+    }
+
+    public boolean applyValue(Object value) {
+        return false;
+    }
+
+}
